fe0f98c9b81c25caa5c2a56e234640f1a0f8d55f
RobertZ00 <zachwiejar@student.agh.edu.pl>
2020-12-15 21:32:44 +0100

    uruchomiona biblioteka sfml

--- ProjektPI/Źródło.cpp
+++ ProjektPI/Źródło.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-//#include <SFML/window.h>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 
 using namespace std;
 
 int main()
 {
-	
+	sf::RenderWindow window{ sf::VideoMode(800, 600, 32), "Dinozaur" };
+
+	while (window.isOpen())
+	{
+		sf::Event windowEvent;
+		while (window.pollEvent(windowEvent))
+		{
+			if (windowEvent.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+		window.clear(sf::Color::White);
+		window.display();
+	}
 }
